0af23924219cf2b4d66ffdbcef3d54943e15bd6a
Bora <bborayrdmc@gmail.com>
2026-05-17 22:42:10 +0300

    feat(components): footer href's open new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,9 +4,9 @@ export function Footer(){
 
         <div className="text-white flex flex-row items-center gap-10">
 
-            <a className="hover:text-blue-500 hover:underline" href="https://github.com/borayrdmc">My GitHub</a>
-            <a className="hover:text-blue-500 hover:underline" href="mailto:bborayrdmc@gmail.com">My Mail</a>
-            <a className="hover:text-blue-500 hover:underline" href="https://linkedin.com/in/gokhanpercem">My LinkedIn</a>
+            <a className="hover:text-blue-500 hover:underline" target="_blank" rel="noopener" href="https://github.com/borayrdmc">My GitHub</a>
+            <a className="hover:text-blue-500 hover:underline" target="_blank" rel="noopener" href="<mailto>:bborayrdmc@gmail.com">My Mail</a>
+            <a className="hover:text-blue-500 hover:underline" target="_blank" rel="noopener" href="https://linkedin.com/in/gokhanpercem">My LinkedIn</a>
             <p>© Bora Yardımcı</p>
         </div>
     );
