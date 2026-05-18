8be954c87ec989fc954eaf5304e59a0904f50002
Bora <bborayrdmc@gmail.com>
2026-05-18 21:19:12 +0300

    feat(components): add error messages via toast

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -5,25 +5,18 @@ import { SearchBoxTypes } from "@/types/SearchBoxTypes";
 import { CityNameNormalizer } from "@/utils/CityNameNormalizer";
 import { CombinedDataTypes } from "@/types/CombinedDataTypes";
 import { cn } from "@/utils/TailwindMerge";
+import toast from "react-hot-toast";
 
 function SearchBox({setWeatherOnSearch,setLocationOnSearch,classname}: SearchBoxTypes){ //Searchbox takes a function as parameter and this function has to follow search box type rules
 
     const [cityNameInput,setCityNameInput]=useState("");
     const [isInputLoading,setIsInputLoading]=useState(false);
-    const [error,setError]=useState<string|null>(null);
 
     const handleSearch= async ()=>{
-        
-        if (!cityNameInput.trim()){
-            setError("City name can't be blank."); //If entry is empty 
-            return;
-        }
 
         const input=cityNameInput;
         setCityNameInput(""); //Reset input 
-
         setIsInputLoading(true);
-        setError(null);
 
         try{
 
@@ -32,18 +25,16 @@ function SearchBox({setWeatherOnSearch,setLocationOnSearch,classname}: SearchBox
             const response = await fetch(`/api/weather?city=${encodeURIComponent(normalizedCityName)}`); //Send a fetch request to api/weather router and has a city parameter for geocoding
             
             if(!response.ok){
-                const err=await response.json(); //Error json coming from router
-                setError(err.error); //Error type coming from Internal Server or Router (like "City not found: Atlantis" | "Internal Server Error" | "city parameter is required")
-                return;
+                const errorData = await response.json();
+                throw new Error(errorData.message);
             }
 
             const data = await response.json() as CombinedDataTypes; //Fetch succesfull // Değiştir
             setWeatherOnSearch(data.weather); //Use data on incoming function (which will be setWeather(data))
             setLocationOnSearch(data.location);
         }
-        catch(error){
-            console.log(error);
-            setError("Network error occured."); //For failed fetch requests
+        catch(error:any){
+            toast.error(error.message)
         }
         finally{
             setIsInputLoading(false);
--- src/components/StartupPage.tsx
+++ src/components/StartupPage.tsx
@@ -1,70 +1,76 @@
-    import { StartupPageTypes } from "@/types/StartupPageTypes";
-    import SearchBox from "./SearchBox";
-    import { Footer } from "./Footer";
-    import { FaMapMarkerAlt } from "react-icons/fa";
-    import { getCurrentLocation } from "@/utils/GetCurrentLocation";
+import { StartupPageTypes } from "@/types/StartupPageTypes";
+import SearchBox from "./SearchBox";
+import { Footer } from "./Footer";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { getCurrentLocation } from "@/utils/GetCurrentLocation";
 import { CurrentLocationButton } from "./CurrentLocationButton";
+import toast from "react-hot-toast";
 
-    export function StartupPage({setWeatherData,setLocationData}: StartupPageTypes){
+export function StartupPage({setWeatherData,setLocationData}: StartupPageTypes){
 
-        function handleCLick(){
+    function handleCLick(){
 
-            getCurrentLocation(
+        getCurrentLocation(
+            
+            async function(lat, lon){
                 
-                async function(lat, lon){
-                    
-                    const response = await fetch(`/api/weather?lat=${lat}&lon=${lon}`);
-                    const data = await response.json();
+                const response = await fetch(`/api/weather?lat=${lat}&lon=${lon}`);
+                const data = await response.json();
 
-                    if (data.weather && data.location){
-                        setWeatherData(data.weather);
-                        setLocationData(data.location);
-                    }
+                if(!response.ok){
+                    const errorData=data;
+                    toast.error(errorData.message);
                 }
-            );
-        }
 
-        return(
-
-            <div className="w-full min-h-screen bg-[#131314] flex flex-col items-center justify-between p-8 relative overflow-hidden">
-                
-                <div className="absolute top-[-10%] left-[-10%] w-[40%] h-[40%] bg-blue-500/5 blur-[120px] rounded-full" />
-                <div className="absolute top-[-10%] right-[-10%] w-[40%] h-[40%] bg-blue-500/5 blur-[120px] rounded-full" />
-
-
-                <div className="flex-1 flex flex-col items-center justify-center w-full max-w-5xl mx-auto">
-                    
-                    <div className="text-center mb-12">
-                        <h1 className="text-8xl md:text-9xl text-white font-black">Weatherly</h1>
-                    </div>
-
-
-                    <div className="w-full max-w-3xl flex flex-col items-center gap-10">
-
-                        <div className="flex flex-col gap-4 text-center">
-
-                            <h2 className="text-3xl text-white font-medium">Ready to explore the <span className="text-gray-400">skies?</span></h2>
-                            <p className="text-gray-500 text-lg max-w-xl mx-auto font-light">Detailed weather forecasts for<span className="text-white text-lg font-bold"> over 250.000</span> cities. Just type, search and discover <span className="text-white text-lg font-bold">all around the world.</span> Try it yourself.</p>
-                        </div>
-
-                        <div className="w-full flex flex-col items-center gap-6">
-
-                            <div className="w-full relative">
-                                <SearchBox classname="py-2 px-1" setWeatherOnSearch={setWeatherData} setLocationOnSearch={setLocationData}/>
-                            </div>
-
-                            <CurrentLocationButton onClickFunction={handleCLick}/>
-                        </div>
-                    </div>
-
-
-                    <div className="mt-16 flex flex-col items-center opacity-60">
-                        <p className="text-gray-400 text-xs">Trusted by <span className="text-white font-bold">2</span> users worldwide :)</p>
-                    </div>
-
-                </div>
-
-                <Footer/>
-            </div>
+                else if (data.weather && data.location){
+                    setWeatherData(data.weather);
+                    setLocationData(data.location);
+                }
+            }
         );
     }
+
+    return(
+
+        <div className="w-full min-h-screen bg-[#131314] flex flex-col items-center justify-between p-8 relative overflow-hidden">
+            
+            <div className="absolute top-[-10%] left-[-10%] w-[40%] h-[40%] bg-blue-500/5 blur-[120px] rounded-full" />
+            <div className="absolute top-[-10%] right-[-10%] w-[40%] h-[40%] bg-blue-500/5 blur-[120px] rounded-full" />
+
+
+            <div className="flex-1 flex flex-col items-center justify-center w-full max-w-5xl mx-auto">
+                
+                <div className="text-center mb-12">
+                    <h1 className="text-8xl md:text-9xl text-white font-black">Weatherly</h1>
+                </div>
+
+
+                <div className="w-full max-w-3xl flex flex-col items-center gap-10">
+
+                    <div className="flex flex-col gap-4 text-center">
+
+                        <h2 className="text-3xl text-white font-medium">Ready to explore the <span className="text-gray-400">skies?</span></h2>
+                        <p className="text-gray-500 text-lg max-w-xl mx-auto font-light">Detailed weather forecasts for<span className="text-white text-lg font-bold"> over 250.000</span> cities. Just type, search and discover <span className="text-white text-lg font-bold">all around the world.</span> Try it yourself.</p>
+                    </div>
+
+                    <div className="w-full flex flex-col items-center gap-6">
+
+                        <div className="w-full relative">
+                            <SearchBox classname="py-2 px-1" setWeatherOnSearch={setWeatherData} setLocationOnSearch={setLocationData}/>
+                        </div>
+
+                        <CurrentLocationButton onClickFunction={handleCLick}/>
+                    </div>
+                </div>
+
+
+                <div className="mt-16 flex flex-col items-center opacity-60">
+                    <p className="text-gray-400 text-xs">Trusted by <span className="text-white font-bold">2</span> users worldwide :)</p>
+                </div>
+
+            </div>
+
+            <Footer/>
+        </div>
+    );
+}
