df900042081136ef697456f7c20c4c4f7724f69c
Bora <bborayrdmc@gmail.com>
2026-05-20 22:35:59 +0300

    feat(components): better current location button

--- src/components/CurrentLocationButton.tsx
+++ src/components/CurrentLocationButton.tsx
@@ -6,9 +6,9 @@ export function CurrentLocationButton({onClickFunction}:{onClickFunction:()=>voi
         
         <button 
             onClick={onClickFunction} 
-            className="flex flex-row items-center gap-2 px-6 py-3 rounded-full transition-all active:scale-95 cursor-pointer bg-slate-400 text-black dark:bg-[#1E1F20] dark:hover:bg-[#252729] dark:text-white">
+            className="flex flex-row items-center gap-2 px-6 py-3 rounded-full transition-all active:scale-95 cursor-pointer border shadow-sm border-slate-200/50 hover:border-slate-300/80 bg-white/70 text-slate-900 dark:text-slate-200 dark:border-white/8 dark:hover:border-white/15 dark:shadow-gray-700/15 dark:bg-[#1E1F20]/70">
 
-            <FaMapMarkerAlt className="text-blue-500"/><span className="text-sm">Use current location</span>
+            <FaMapMarkerAlt className="text-slate-900 dark:text-slate-200"/><span className="text-sm">Use current location</span>
         </button>
     )
 }
