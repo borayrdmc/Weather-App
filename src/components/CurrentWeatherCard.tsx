684976ab52558f46364ca9022f74910fa0f47ce6
Bora <bborayrdmc@gmail.com>
2026-05-25 18:32:16 +0300

    feat(components): current weather card responsive layout

--- src/components/CurrentWeatherCard.tsx
+++ src/components/CurrentWeatherCard.tsx
@@ -18,22 +18,25 @@ export default function CurrentWeatherCard({currentWeatherData,onClickFunction,u
 
     return(
 
-        <div onClick={()=>onClickFunction(true)} className="w-full max-w-4xl flex flex-row flex-wrap justify-between rounded-2xl p-5 cursor-pointer transition-all duration-200 backdrop-blur-md shadow-sm border border-slate-200/50 hover:border-slate-300/80 bg-white/70 dark:border dark:border-white/8 dark:hover:border-white/15 dark:shadow-gray-700/15 dark:bg-[#1E1F20]/70">
+        <div onClick={()=>onClickFunction(true)} className="w-full max-w-4xl flex flex-col gap-2 lg:flex-row justify-between p-5 rounded-2xl cursor-pointer transition-all duration-200 backdrop-blur-md shadow-sm border border-slate-200/50 hover:border-slate-300/80 bg-white/70 dark:border dark:border-white/8 dark:hover:border-white/15 dark:shadow-gray-700/15 dark:bg-[#1E1F20]/70">
 
-            <div className="flex flex-row items-center gap-5">
+            <p className="text-xl text-center lg:hidden">{dayName} {languageDictionary.currentweather.cardtitledaily}</p>
+            
+            <div className="flex flex-col items-center lg:flex-row gap-2 lg:gap-5">
 
-                <img className="w-40 h-40 object-contain" src={iconPath} alt="current weather image"></img>
+                <div className="flex flex-row gap-10 lg:gap-5">
+                    <img className="w-30 h-30 lg:w-40 lg:h-40 object-contain" src={iconPath} alt="current weather image"></img>
 
-                <div className="flex flex-row items-center">
-                    <p className="text-7xl font-bold leading-none p-0">{UnitConverter(currentWeatherData.temp,unit)}</p>
+                    <div className="flex flex-row items-center justify-center">
+                        <p className="text-7xl lg:text-7xl font-bold leading-none p-0">{UnitConverter(currentWeatherData.temp,unit)}</p>
 
-                    <div className="flex flex-col">
-                        {unit !== "K" && <p className="text-5xl font-bold leading-none">°</p>}
-                        <p className="mx-2">{unit}</p>
+                        <div className="flex flex-col">
+                            {unit !== "K" && <p className="text-6xl lg:text-5xl font-bold leading-none">°</p>}
+                            <p className="mx-2">{unit}</p>
+                        </div>
                     </div>
                 </div>
-                
-                <div className="flex flex-col items-start text-slate-700 dark:text-slate-100">
+                <div className="flex flex-col items-center lg:justify-center text-slate-700 dark:text-slate-100">
                     <p className="text-lg">{languageDictionary.currentweather.uv_index}: {currentWeatherData.uvi}</p>
                     <p className="text-lg">{languageDictionary.currentweather.wind_speed}: {currentWeatherData.wind_speed} m/s</p>
                     <p className="text-lg">{languageDictionary.currentweather.humidity}: {currentWeatherData.humidity}%</p>
@@ -41,7 +44,7 @@ export default function CurrentWeatherCard({currentWeatherData,onClickFunction,u
                 </div>
             </div>
 
-            <div className="flex flex-col text-right">
+            <div className="hidden sm:flex flex-col text-right">
                 <p className="font-bold text-2xl">{languageDictionary.currentweather.cardtitledaily}</p>
                 <p className="text-lg">{dayName}</p>
                 <p className="text-lg capitalize">{currentWeatherData.weather[0].description}</p>
