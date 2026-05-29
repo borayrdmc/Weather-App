cdf77779e470f935c82beb02ca8697f7724210fe
Bora <bborayrdmc@gmail.com>
2026-05-29 18:00:47 +0300

    feat(components): adjust settings page

--- src/components/settings/SettingsPage.tsx
+++ src/components/settings/SettingsPage.tsx
@@ -3,6 +3,7 @@ import { UnitButtons } from "./UnitButtons";
 import { TemperatureUnit } from "@/types/WeatherTypes";
 import { ThemeButtons } from "./ThemeButtons";
 import { LanguageButtons } from "./LanguageButtons";
+import { useLanguage } from "@/contexts/LanguageContext";
 
 interface SettingsTypes{
     onClickFunction:(bool:boolean)=>void;
@@ -11,6 +12,7 @@ interface SettingsTypes{
     theme:string|undefined;
     setTheme:(theme: string)=>void;
 }
+
 
 export function SettingsPage({onClickFunction,unitSetter,unit,theme,setTheme}:SettingsTypes){
 
@@ -30,16 +32,17 @@ export function SettingsPage({onClickFunction,unitSetter,unit,theme,setTheme}:Se
 
     },[onClickFunction]);
 
-
+    const {languageDictionary,language} = useLanguage();
 
     return(
 
         <div className="fixed inset-0 z-50 flex flex-col items-center justify-center backdrop-blur-md bg-slate-500/10 dark:bg-black/50 " onClick={()=>onClickFunction(false)}>
         
-            <div className="relative w-full max-w-lg flex flex-col gap-4 items-center p-4 rounded-lg border bg-slate-100 shadow-sm border-slate-200/50 dark:border dark:bg-[#131314] dark:shadow-gray-700/15 dark:border-white/8" onClick={(e)=>e.stopPropagation()}>
+            <div className="relative flex flex-col gap-4 p-5 items-center rounded-lg border bg-slate-100 shadow-sm border-slate-200/50 dark:border dark:bg-[#131314] dark:shadow-gray-700/15 dark:border-white/8" onClick={(e)=>e.stopPropagation()}>
 
                 <button className="absolute top-2 right-4  cursor-pointer text-xl font-semibold transition-colors duration-200 text-gray-700 dark:text-gray-400 dark:hover:text-white" onClick={()=>onClickFunction(false)}>✕</button>
 
+                <p className="font-medium text-xl">{languageDictionary.settings.settings}</p>
                 <UnitButtons unitSetter={unitSetter} unit={unit}/>
                 <ThemeButtons theme={theme} themeSetter={setTheme}/>
                 <LanguageButtons/>
--- src/components/settings/UnitButtons.tsx
+++ src/components/settings/UnitButtons.tsx
@@ -6,15 +6,16 @@ interface UnitButtonsTypes{
     unitSetter:(unit:"C"|"K"|"F")=>void;
 }
 
+// <button className={`${BASE_TEMPLATE} ${unit === "K" ? ACTIVE_TEMPLATE : NOT_ACTIVE_TEMPLATE}`} onClick={()=>unitSetter("K")}>Kelvin</button> //Kelvin Button
+
 export function UnitButtons({unitSetter,unit}:UnitButtonsTypes){
 
     return(
         
-        <div className="grid grid-cols-3 gap-3">
+        <div className="grid grid-cols-2 gap-3">
 
             <button className={`${BASE_TEMPLATE} ${unit === "C" ? ACTIVE_TEMPLATE : NOT_ACTIVE_TEMPLATE}`} onClick={()=>unitSetter("C")}>Celcius</button>
             <button className={`${BASE_TEMPLATE} ${unit === "F" ? ACTIVE_TEMPLATE : NOT_ACTIVE_TEMPLATE}`} onClick={()=>unitSetter("F")} >Fahrenheit</button>
-            <button className={`${BASE_TEMPLATE} ${unit === "K" ? ACTIVE_TEMPLATE : NOT_ACTIVE_TEMPLATE}`} onClick={()=>unitSetter("K")}>Kelvin</button>
         </div>
     )
 }   
